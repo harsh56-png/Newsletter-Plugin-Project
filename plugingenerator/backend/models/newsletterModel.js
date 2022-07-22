1ec4eb7ecda441f8a5d42d5643cd5fca56b9c3c9
harsh56-png <harshsrivastava870@gmail.com>
2022-07-22 22:39:04 +0530

    newsletter input showing in mongo

--- plugingenerator/backend/models/newsletterModel.js
+++ plugingenerator/backend/models/newsletterModel.js
@@ -1,6 +1,7 @@
 const {Schema, model, Types} = require('../connection');   // importing connection.js
 
 const ob = new Schema({
+    email: String,
     owner: {type: Types.ObjectId, ref: 'Users'},
     content: String,
     createdAt: Date,
